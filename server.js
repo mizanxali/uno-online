93d0c9fdde4517aa38ea1396fca759159a9a67a6
Mizanali Panjwani <mizanalip786@gmail.com>
2021-02-22 05:29:23 +0530

    Set up socketio server-client connection

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(cors())
+
+io.on('connection', socket => {
+    console.log('connection made')
+
+    socket.on('disconnect', () => {
+        console.log('connection lost')
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
