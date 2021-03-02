b2c3fd2e81c786e0c65b6a1757784ef6aff872e5
Mizanali Panjwani <mizanalip786@gmail.com>
2021-03-02 15:07:36 +0530

    Deployment setup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const socketio = require('socket.io')
 const http = require('http')
 const cors = require('cors')
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
@@ -50,6 +51,15 @@ io.on('connection', socket => {
             io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
     })
 })
+
+//serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+	//set static folder
+	app.use(express.static('client/build'))
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
